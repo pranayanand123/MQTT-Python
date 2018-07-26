496aebe098afb5f8a6e13cd89397af34e1eb5be5
Pranay Anand <pranayanand123@gmail.com>
2018-07-26 11:28:29 +0530

    Add files via upload

---
+++ mqtt_pub.py
@@ -0,0 +1,10 @@
+import paho.mqtt.client as paho
+broker="broker.mqttdashboard.com"
+port=1883
+def on_publish(client,userdata,result):             #create function for callback
+    print("data published \n")
+    pass
+client1= paho.Client("control1")                           #create client object
+client1.on_publish = on_publish                          #assign function to callback
+client1.connect(broker,port)                                 #establish connection
+ret= client1.publish("ronaldo","11:18 a.m")                   #publish
